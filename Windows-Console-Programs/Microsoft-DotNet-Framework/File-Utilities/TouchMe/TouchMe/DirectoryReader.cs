819ccd7f395368600ffdb2076f165ed9ec083267
Zachary Burns <developer@zackburns.com>
2016-10-11 18:30:35 -0400

    Add files via upload

---
+++ Windows-Console-Programs/Microsoft-DotNet-Framework/File-Utilities/TouchMe/TouchMe/DirectoryReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TouchMe
+{
+    class DirectoryReader : System.Collections.ArrayList
+    {
+        bool m_recursive;
+        System.Text.RegularExpressions.Regex m_selectionPattern;
+
+        public DirectoryReader(bool isRecursive, string selectionPattern)
+        {
+            this.m_recursive = isRecursive;
+            this.m_selectionPattern = new System.Text.RegularExpressions.Regex("^" + System.Text.RegularExpressions.Regex.Escape(selectionPattern).Replace(@"\*", ".*").Replace(@"\?", ".") + "$");
+        }
+
+        public void collectFiles(string path)
+        {
+            Console.WriteLine("Collecting files for " + path + ". Please wait...\n");
+            string[] files = System.IO.Directory.GetFiles(path);
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (this.isEligible(files[i]))
+                {
+                    this.Add(files[i]);
+                }
+            }
+            if (this.m_recursive)
+            {
+                string[] directories = System.IO.Directory.GetDirectories(path);
+                for (int j = 0; j < directories.Length; j++)
+                {
+                    this.collectFiles(directories[j]);
+                }
+            }
+        }
+
+
+        private bool isEligible(string filename)
+        {
+            return this.m_selectionPattern.IsMatch(filename);
+        }
+
+ 
+
+ 
+
+    }
+}
---
+++ Windows-Console-Programs/Microsoft-DotNet-Framework/File-Utilities/TouchMe/TouchMe/Touch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TouchMe
+{
+    class Touch
+    {
+        string m_pattern;
+        string m_root;
+        bool m_subdirs;
+
+        public Touch(string root, string pattern, bool subdirs)
+        {
+            this.m_root = root;
+            this.m_pattern = pattern;
+            this.m_subdirs = subdirs;
+        }
+
+        public void Go(DateTime when)
+        {
+            DirectoryReader reader = new DirectoryReader(this.m_subdirs, this.m_pattern);
+            reader.collectFiles(this.m_root);
+            foreach (string str in reader)
+            {
+                this.touchFile(str, when);
+            }
+        }
+
+        public void touchFile(string filename, DateTime when)
+        {
+            string str = "";
+            try
+            {
+                if (System.IO.File.Exists(filename))
+                {
+                    System.IO.File.SetLastWriteTime(filename, when);
+                    str = "SET: " + filename;
+                }
+                else
+                {
+                    str = string.Format("\nERROR: Cannot find file " + filename + "\n", new object[0]);
+                }
+            }
+            catch (Exception exception)
+            {
+                str = string.Format("\nERROR: Cannot touch file {0}: {1}\n", str, exception.Message);
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
